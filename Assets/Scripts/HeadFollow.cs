d74b1b32d6004ebbd9a7c87f1d52fee311cb6afe
Sutton Ashby <sutton.ashby@gmail.com>
2019-10-14 21:55:19 -0400

    init project

---
+++ Assets/Scripts/HeadFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadFollow : MonoBehaviour
+{
+    public bool track = true;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(track){
+            transform.LookAt(target);        
+        }
+    }
+}
